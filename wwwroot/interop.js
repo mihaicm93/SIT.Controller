30ae64234395cde859120016eaea32852590f22f
mihaicm93 <45673304+mihaicm93@users.noreply.github.com>
2024-06-04 00:16:42 +0200

    Game Server Folder selection

---
+++ wwwroot/interop.js
@@ -0,0 +1,15 @@
+﻿window.showFolderPicker = async () => {
+    try {
+        const pickerOpts = {
+            types: [],
+            excludeAcceptAllOption: true,
+            multiple: false
+        };
+
+        const handle = await window.showDirectoryPicker(pickerOpts);
+        return handle.name;
+    } catch (err) {
+        console.error(err);
+        return null;
+    }
+};
