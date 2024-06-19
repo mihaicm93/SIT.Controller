3a3749b0c27ecef3d53a7bd770a38a3c6f007dd9
mihaicm93 <45673304+mihaicm93@users.noreply.github.com>
2024-06-19 18:39:24 +0200

    1st Implementation of Email Sender through Mailgun API

---
+++ Areas/EmailSender/MailgunOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace SIT.Controller.Areas.EmailSender
+{
+    public class MailgunOptions
+    {
+        public string? ApiKey { get; set; }
+        public string? Domain { get; set; }
+    }
+}
